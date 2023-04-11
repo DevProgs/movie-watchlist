c7b7db4785ae67a63ade63f901f9f4048b13d4a3
DevProgs <DevProgs1@gmail.com>
2023-04-11 02:47:46 -0500

    Saved movie data to localstorage

--- main.js
+++ main.js
@@ -1,24 +1,4 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+let data = JSON.parse(localStorage.getItem("movie"))
+console.log(data);
+// seachMovie()
+document.getElementById('app').innerHTML = `<img src=${data.Poster} alt="movie poster"/>`
