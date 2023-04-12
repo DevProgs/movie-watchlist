d7092b6591220a497798d70232bd39415bd5bedd
DevProgs <DevProgs1@gmail.com>
2023-04-12 01:09:49 -0500

    Added header image and content

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-let data = JSON.parse(localStorage.getItem("movie"))
-console.log(data);
-// seachMovie()
-document.getElementById('app').innerHTML = `<img src=${data.Poster} alt="movie poster"/>`
+// let data = JSON.parse(localStorage.getItem("movie"))
+// console.log(data);
+// document.getElementById('app').innerHTML = `<img src=${data.Poster} alt="movie poster"/>`
