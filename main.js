6cbcf0ce1ffbd75e65732c648042a7e21ca82a79
DevProgs <DevProgs1@gmail.com>
2023-04-18 03:42:57 -0500

    Removed some HTML

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 // let data = JSON.parse(localStorage.getItem("movie"))
 // console.log(data);
 // document.getElementById('app').innerHTML = `<img src=${data.Poster} alt="movie poster"/>`
+const movies = document.querySelector('.movies')
