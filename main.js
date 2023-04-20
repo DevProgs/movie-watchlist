0e8017ad087321e04759b5ffefa188578c1a2c74
DevProgs <DevProgs1@gmail.com>
2023-04-20 01:07:17 -0500

    setup search function to api

--- main.js
+++ main.js
@@ -2,3 +2,14 @@
 // console.log(data);
 // document.getElementById('app').innerHTML = `<img src=${data.Poster} alt="movie poster"/>`
 const movies = document.querySelector('.movies')
+const searchMovie = document.querySelector('.search')
+const searchBtn = document.querySelector('.search-btn')
+
+const getSearchedMovie = () => {
+  fetch(`http://www.omdbapi.com/?s=${searchMovie.value}&apikey=80010bb0`)
+    .then((res) => res.json())
+    .then((data) => 
+    data.Search.map((movie) => console.log(movie.Title))
+    );
+}
+searchBtn.addEventListener('click', getSearchedMovie);
