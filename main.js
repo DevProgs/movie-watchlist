3f0aee66df1e6b9caf956553465c45ed8b2432d0
DevProgs <DevProgs1@gmail.com>
2023-04-20 02:33:51 -0500

    Added array to push movie data into

--- main.js
+++ main.js
@@ -4,12 +4,30 @@
 const movies = document.querySelector('.movies')
 const searchMovie = document.querySelector('.search')
 const searchBtn = document.querySelector('.search-btn')
+const movieSection = document.querySelector('.movies');
+let searchedMovieHTML = ''
+const moviesArray = []
 
 const getSearchedMovie = () => {
   fetch(`http://www.omdbapi.com/?s=${searchMovie.value}&apikey=80010bb0`)
     .then((res) => res.json())
-    .then((data) => 
-    data.Search.map((movie) => console.log(movie.Title))
-    );
+    .then((data) => data.Search.forEach((movie) => {
+      fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=80010bb0`)
+        .then(res => res.json())
+        .then(data => moviesArray.push({
+          title: data.Title,
+          poster: data.Poster,
+          rating: data.imdbRating,
+          runtime: data.Runtime,
+          genre: data.Genre,
+          plot: data.Plot
+        }));
+    }
+  ))
+  console.log(moviesArray)
 }
+
+
+
+
 searchBtn.addEventListener('click', getSearchedMovie);
