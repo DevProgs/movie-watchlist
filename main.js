f533f4f78ffdd28305117e90103cf9776829acb3
DevProgs <DevProgs1@gmail.com>
2023-04-21 01:38:17 -0500

    added code to hide explore image on fetch

--- main.js
+++ main.js
@@ -5,27 +5,9 @@ const movies = document.querySelector('.movies')
 const searchMovie = document.querySelector('.search')
 const searchBtn = document.querySelector('.search-btn')
 const movieSection = document.querySelector('.movies');
-let searchedMovieHTML = ''
+const exploreImage = document.querySelector('.start-exploring')
+// let searchedMovieHTML = ''
 const moviesArray = []
-
-// const getSearchedMovie = () => {
-//   fetch(`http://www.omdbapi.com/?s=${searchMovie.value}&apikey=80010bb0`)
-//     .then((res) => res.json())
-//     .then((data) => data.Search.forEach((movie) => {
-//       fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=80010bb0`)
-//         .then(res => res.json())
-//         .then(data => moviesArray.push({
-//           title: data.Title,
-//           poster: data.Poster,
-//           rating: data.imdbRating,
-//           runtime: data.Runtime,
-//           genre: data.Genre,
-//           plot: data.Plot
-//         }));
-//     }
-//   ))
-//   console.log(moviesArray)
-// }
 
 const getSearchedMovie = () => {
   fetch(`http://www.omdbapi.com/?s=${searchMovie.value}&apikey=80010bb0`)
@@ -36,6 +18,7 @@ const getSearchedMovie = () => {
 }
 
 const getMovieID = (ID) => {
+  exploreImage.style.display = 'none';
   fetch(`http://www.omdbapi.com/?i=${ID}&apikey=80010bb0`)
     .then((res) => res.json())
     .then(
