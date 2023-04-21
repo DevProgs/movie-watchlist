7eae1caffd3a099cb11459f59afa20a8763bbb7f
DevProgs <DevProgs1@gmail.com>
2023-04-21 01:32:17 -0500

    added function to display movie info by ID

--- main.js
+++ main.js
@@ -8,26 +8,65 @@ const movieSection = document.querySelector('.movies');
 let searchedMovieHTML = ''
 const moviesArray = []
 
+// const getSearchedMovie = () => {
+//   fetch(`http://www.omdbapi.com/?s=${searchMovie.value}&apikey=80010bb0`)
+//     .then((res) => res.json())
+//     .then((data) => data.Search.forEach((movie) => {
+//       fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=80010bb0`)
+//         .then(res => res.json())
+//         .then(data => moviesArray.push({
+//           title: data.Title,
+//           poster: data.Poster,
+//           rating: data.imdbRating,
+//           runtime: data.Runtime,
+//           genre: data.Genre,
+//           plot: data.Plot
+//         }));
+//     }
+//   ))
+//   console.log(moviesArray)
+// }
+
 const getSearchedMovie = () => {
   fetch(`http://www.omdbapi.com/?s=${searchMovie.value}&apikey=80010bb0`)
-    .then((res) => res.json())
-    .then((data) => data.Search.forEach((movie) => {
-      fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=80010bb0`)
-        .then(res => res.json())
-        .then(data => moviesArray.push({
-          title: data.Title,
-          poster: data.Poster,
-          rating: data.imdbRating,
-          runtime: data.Runtime,
-          genre: data.Genre,
-          plot: data.Plot
-        }));
-    }
-  ))
-  console.log(moviesArray)
+    .then(res => res.json())
+    .then(data => data.Search.forEach((movie) => {
+      getMovieID(movie.imdbID)
+    }));
 }
 
-
+const getMovieID = (ID) => {
+  fetch(`http://www.omdbapi.com/?i=${ID}&apikey=80010bb0`)
+    .then((res) => res.json())
+    .then(
+      (data) =>
+        (movieSection.innerHTML += `
+    <div class="movie">
+      <img class="poster" src="${data.Poster}" alt="${data.Title}">
+      <div class="movie-info">
+          <div class="title-container">
+            <h3>${data.Title}</h3>
+            <img class="star" src="/images/star.png" alt="star">
+            <p class="movie-rating">${data.imdbRating}</p>
+          </div>
+          <div class="genre-container">
+            <p>${data.Runtime}</p>
+            <p>${data.Genre}</p>
+            <div class="watchlist-btn-container">
+              <button class="add-btn">
+                <img class="plus-icon" src="/images/plus.svg" alt="plus">
+              </button>
+              <p>Watchlist</p>
+            </div>
+          </div>
+          <p class="movie-description">${data.Plot}</p>
+        </div>
+      </div>
+      <div class="container divider"></div>
+    </div>
+    `)
+    );
+};
 
 
 searchBtn.addEventListener('click', getSearchedMovie);
